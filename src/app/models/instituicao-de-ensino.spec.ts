3c48783aa015822347b3e5a8155d0eefd8bc72da
KaueSanchesG <kauesanches123@gmail.com>
2023-11-02 15:55:27 -0300

    feat: structuring models folder

---
+++ src/app/models/instituicao-de-ensino.spec.ts
@@ -0,0 +1,7 @@
+import { InstituicaoDeEnsino } from './instituicao-de-ensino';
+
+describe('InstituicaoDeEnsino', () => {
+  it('should create an instance', () => {
+    expect(new InstituicaoDeEnsino()).toBeTruthy();
+  });
+});
---
+++ src/app/models/ubs.spec.ts
@@ -0,0 +1,7 @@
+import { Ubs } from './ubs';
+
+describe('Ubs', () => {
+  it('should create an instance', () => {
+    expect(new Ubs()).toBeTruthy();
+  });
+});
