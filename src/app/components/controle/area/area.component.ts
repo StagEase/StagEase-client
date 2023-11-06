d9cf5c67082e5172752bd8b21670e01d306be0ff
marcelonv011 <marcelonv011@gmail.com>
2023-11-06 18:28:03 -0300

    area atualizar pronto

--- src/app/components/controle/area/area.component.ts
+++ src/app/components/controle/area/area.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Area } from 'src/app/models/area';
 import { AreaService } from 'src/app/services/area.service';
 
@@ -12,11 +12,15 @@ export class AreaComponent {
   @Output() retorno = new EventEmitter<Area>();
   @Input() modoLancamento: boolean = false;
 
+  objetoSelecionadoParaEdicao: Area = new Area();
+  indiceSelecionadoParaEdicao!: number;
+
   list: Area[] = [];
   areaSelecionada: Area = new Area();
 
   service = inject(AreaService);
   modal = inject(NgbModal);
+  modalRef!: NgbModalRef;
 
   constructor() {
     this.listAll();
@@ -48,6 +52,18 @@ export class AreaComponent {
     this.areaSelecionada = area;
     console.log('Equipamento selecionado:', this.areaSelecionada);
     this.modal.open(modal, { size: 'lg' });
+  }
+
+  editar(modal: any, area: Area, indice: number) {
+    this.objetoSelecionadoParaEdicao = { ...area };
+    this.indiceSelecionadoParaEdicao = indice;
+
+    this.modalRef = this.modal.open(modal, { size: 'md' });
+  }
+
+  addOuEditarArea(area: Area) {
+    this.listAll();
+    this.modal.dismissAll();
   }
 
   deletarArea() {
