9bd0fa375126e92217f82b93f1bae972f0b997ff
ppedroandrade <pedrosalvaro09@gmail.com>
2023-11-13 10:59:13 -0300

    FIX: Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,29 +6,24 @@ import { SupervisorComponent } from './components/controle/supervisor/supervisor
 import { InstituicaoComponent } from './components/controle/instituicao/instituicao.component';
 import { EquipamentoComponent } from './components/controle/equipamento/equipamento.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
     component: IndexComponent,
     children: [
-      {
-        path: 'controle',
-        children: [
-          { path: 'area', component: AreaComponent },
-          { path: 'supervisor', component: SupervisorComponent },
-          { path: 'equipamento', component: EquipamentoComponent },
-          { path: 'instituicao', component: InstituicaoComponent },
-        ],
-      },
-      {
-        path: 'home',
-        component: HomeComponent,
-      },
+      { path: '', component: HomeComponent },
+      { path: 'area', component: AreaComponent },
+      { path: 'supervisor', component: SupervisorComponent },
+      { path: 'equipamento', component: EquipamentoComponent },
+      { path: 'instituicao', component: InstituicaoComponent },
     ],
   },
-
-  { path: 'saiu', component: IndexComponent },
 ];
 
 @NgModule({
